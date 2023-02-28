ffa9aa077740b2baf44a65ce116ba3df078cdaf4
João Pedro <jotapedro1231@gmail.com>
2023-02-28 05:04:16 -0300

    Added styles for Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      /* Bordas criadas para não mover o icone quando der o hover */
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom-color: ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
