ac8b4b30ad5b40d6e2b64057d2bc0944879dc90e
João Pedro <jotapedro1231@gmail.com>
2023-02-28 03:16:54 -0300

    First commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles"
+
+interface ButtonProps {
+    variant?: ButtonVariant
+}
+
+export function Button({ variant: color = 'primary' }: ButtonProps) {
+    return <ButtonContainer variant={color}>Enviar</ButtonContainer>
+}
