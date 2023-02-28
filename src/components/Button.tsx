7bd5d0633f088788db65fd2f44cc691d7b5aa3c0
João Pedro <jotapedro1231@gmail.com>
2023-02-28 03:53:04 -0300

    Added eslint and fixed eslint problems

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles"
+import { ButtonContainer, ButtonVariant } from './Button.styles'
 
 interface ButtonProps {
-    variant?: ButtonVariant
+  variant?: ButtonVariant
 }
 
 export function Button({ variant: color = 'primary' }: ButtonProps) {
-    return <ButtonContainer variant={color}>Enviar</ButtonContainer>
+  return <ButtonContainer variant={color}>Enviar</ButtonContainer>
 }
